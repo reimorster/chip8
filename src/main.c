39554e6ea02acb9a1a84351beb94021dda84cb35
reimorster <reimorster@gmail.com>
2023-03-11 22:34:29 -0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "SDL2/SDL.h"
+
+int main(int argc, char** argv)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow(
+            "Chip8 Window",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            320,
+            SDL_WINDOW_SHOWN);
+
+    while(1);
+
+    printf("Hello world");
+
+    return 0;
+}
